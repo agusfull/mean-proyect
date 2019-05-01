d13d66c153306644aa9933b40a75a487f1868167
Agustin Ezequiel Fullone <agustin.fullone@ibm.com>
2019-05-01 20:21:13 -0300

    Finish user controller and routes

--- routes/user.js
+++ routes/user.js
@@ -1,14 +1,22 @@
 'use strict'
 
 var express = require('express');
-var UserController = require('../controllers/user');
+var UserController = require('../controllers/user'); //Se importa el controlador de usuario
 var api = express.Router();
-var md_auth = require('../middlewares/authenticated');
+var md_auth = require('../middlewares/authenticated'); //Archivo de autenticacion
+var multipart = require('connect-multiparty');
+var md_upload = multipart({ uploadDir: './uploads/users' });
 
 api.get('/probando-controlador', md_auth.ensureAuth, UserController.pruebas);
 
 api.post('/register', UserController.saveUser);
 
 api.post('/login', UserController.loginUser);
+
+api.put('/update-user/:id', md_auth.ensureAuth, UserController.updateUser);
+
+api.post('/upload-image-user/:id', [md_auth.ensureAuth, md_upload], UserController.uploadImage);
+
+api.get('/get-image-user/:imageFile', UserController.getImageFile);
 
 module.exports = api;
